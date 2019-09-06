c7a33516e7f29bfb93b8cd83aa5e473878f589cd
patrick Dos Santos <patrickdos@free.fr>
2019-09-06 20:21:33 +0200

    enhance split job

---
+++ listkey.py
@@ -0,0 +1,55 @@
+from pyspark.sql import SparkSession, Row, SQLContext
+from pyspark import SparkContext
+
+import os
+
+from scality.supervisor import Supervisor
+from scality.daemon import DaemonFactory , ScalFactoryExceptionTypeNotFound
+from scality.key import Key
+from scality.storelib.storeutils import uks_parse
+
+import requests
+requests.packages.urllib3.disable_warnings()
+
+import ssl
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    # Legacy Python that doesn't verify HTTPS certificates by default
+    pass
+else:
+    # Handle target environment that doesn't support HTTPS verification
+    ssl._create_default_https_context = _create_unverified_https_context
+
+spark = SparkSession.builder.getOrCreate()
+"""
+os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages "org.apache.hadoop:hadoop-aws:2.7.3" pyspark-shell'
+sc = SparkContext('local','example')
+
+sc._jsc.hadoopConfiguration().set("fs.s3.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+sc._jsc.hadoopConfiguration().set('fs.s3a.access.key', 'VKIKE9MQ8AM3I5Y0LOZG')
+sc._jsc.hadoopConfiguration().set('fs.s3a.secret.key', 'd1EF3mUbLYBp2oezdzdh37RdQPtXHfmmst0R/zd6')
+sc._jsc.hadoopConfiguration().set('fs.s3a.endpoint', 'http://sreport.scality.com')
+spark = SQLContext(sc)
+"""
+
+def listkeys(row):
+	klist = []
+	n = DaemonFactory().get_daemon("node",login="root", passwd="admin", url='https://{0}:{1}'.format(row.ip, row.adminport), chord_addr=row.ip, chord_port=row.chordport, dso="IT")
+	for k in n.listKeysIter():
+		if len(k.split(",")[0]) > 30 :
+			klist.append(k.rstrip().split(','))
+	return klist
+
+
+s = Supervisor(url="https://sup.scality.com:2443",login="root",passwd="admin")
+listm = sorted(s.supervisorConfigDso(dsoname="IT")['nodes'])
+df = spark.createDataFrame(listm)
+print df.show(36)
+print df.rdd.getNumPartitions()
+#listfullkeys = df.rdd.repartition(36).map(lambda x:listkeys(x))
+listfullkeys = df.rdd.map(lambda x:listkeys(x))
+dfnew = listfullkeys.flatMap(lambda x: x).toDF()
+print dfnew.show(10)
+listkeys = "s3a://spark/listkeys-IT-2.csv" 
+dfnew.write.format('csv').options(header='false').save(listkeys)
--- split.py
+++ split.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession, Row, SQLContext
+import pyspark.sql.functions as F
 from pyspark import SparkContext
 import os
 import requests
@@ -8,7 +9,7 @@ import hashlib
 import binascii
 import time
 
-spark = SparkSession.builder.getOrCreate()
+spark = SparkSession.builder.appName("Check Split Objects").getOrCreate()
 """
 os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages "org.apache.hadoop:hadoop-aws:2.7.3" pyspark-shell'
 sc = SparkContext('local','example')
@@ -19,7 +20,6 @@ sc._jsc.hadoopConfiguration().set('fs.s3a.secret.key', 'd1EF3mUbLYBp2oezdzdh37Rd
 sc._jsc.hadoopConfiguration().set('fs.s3a.endpoint', 'http://sreport.scality.com')
 spark = SQLContext(sc)
 """
-
 def to_bytes(h):
         return binascii.unhexlify(h)
 
@@ -37,14 +37,12 @@ def get_dig_key(name):
             digest[7] << 24 |  digest[8] << 16  | digest[9] << 8 | digest[10]
       hash_str = "{0:x}".format(hash_str)
       oid = "{0:x}".format(oid)
-      print hash_str,oid
       oid = oid.zfill(16)
       volid = "00000000"
       svcid = "51"
       specific = "102060"
       cls = "70"
       key = hash_str.upper() + oid.upper() + volid + svcid + specific + cls
-      print key
       return key.zfill(40)
 
 def gen_md5_from_id(key):
@@ -52,11 +50,20 @@ def gen_md5_from_id(key):
         return get_dig_key(int_b)
 
 def sparse(f):
-	lst  = []	
-	m = re.findall(r'(425a2d44420100000014|80000000002000000000000..)([0-9-a-f]{40})',f)
-	for i in m:
-		lst.append(i[1].upper())
-	return lst
+        lst  = []
+        m = re.findall(r'(200000000000014|20100000014)([0-9-a-f]{40})',f)
+        n = re.findall(r'(200000000000013|20100000013)([0-9-a-f]{38})',f)
+        o = re.findall(r'(200000000000012|20100000012)([0-9-a-f]{36})',f)
+        for mm in m:
+                key = mm[1]
+                lst.append(key.upper())
+        for nn in n:
+                key = nn[1]
+                lst.append(key.upper())
+	for oo in o:
+		key = oo[1]
+		lst.append(key.upper())
+        return lst
 
 def getpath(key):
 	header = {}
@@ -85,36 +92,50 @@ def blob(row):
 		chunkshex =  chunks.encode('hex')
 		rtlst = []
 		for k in list(set(sparse(chunkshex))):
-			rtlst.append({"key":key,"subkey":k,"digkey":gen_md5_from_id(k),"size":video})	
+			rtlst.append({"key":key,"subkey":k,"digkey":gen_md5_from_id(k)[:-14],"size":video})	
 		return rtlst
 	else:	
 		return [{"key":key,"subkey":"KO","digkey":"KO","size":"KO"}]
 
-df = spark.read.format("csv").option("header", "false").option("inferSchema", "true").load("s3a://spark/listmIT-node0[1-6]-n[1-6].csv")
+#df = spark.read.format("csv").option("header", "false").option("inferSchema", "true").load("s3a://spark/list.csv")
+df = spark.read.format("csv").option("header", "false").option("inferSchema", "true").load("s3a://spark/listkeys-IT-5.csv/")
 #df = spark.read.format("csv").option("header", "false").option("inferSchema", "true").load("s3a://video/t.csv")
 #df = spark.read.format("csv").option("header", "false").option("inferSchema", "true").load("s3a://video/all-fixed-light.csv")
 
 dfARCsingle = df.filter(df["_c1"].rlike(r".*000000005.........$") & df["_c3"].rlike("32")).select("_c1").distinct()
-#blob(Row(_c1="4085E9E25998A377965600000000005004DE1420"))
 rdd = dfARCsingle.rdd.map(lambda x : blob(x))
-#print rdd.collect()
 rrdnew = rdd.flatMap(lambda x: x)
-#print rrdnew.collect()
 dfnew = rdd.flatMap(lambda x: x).toDF()
-#print dfnew.show(100)
-single = "s3a://sparkoutput/output-single-%s.csv" % (time.time())
-dfnew.write.format('csv').options(header='false').save(single)
+print dfnew.show(20,False)
+single = "s3a://sparkoutput/output-single.csv"
+#dfnew.write.format('csv').mode("overwrite").options(header='false').save(single)
 
 dfARCSYNC = df.filter(df["_c1"].rlike(r".*000000005.........$") & df["_c3"].rlike("16")).select("_c1").distinct()
-#print dfARCSYNC.show(10)
 
-singlesync = "s3a://sparkoutput/output-single-SYNC-%s.csv" % (time.time())
-dfARCSYNC.write.format('csv').options(header='false').save(singlesync)
+dfARCSYNC = dfARCSYNC.withColumn("_c1",F.expr("substring(_c1, 1, length(_c1)-14)"))
 
-diffDF = dfARCSYNC.subtract(dfnew.select("digkey"))
+print "SYNC", dfARCSYNC.show(20,False)
 
-#INNER JOIN df3 = df1.join(df2, [df1.B == df2.B , df1.C == df2.C], how = 'inner' )
-#print diffDF.show(10)
-diff = "s3a://sparkoutput/output-single-DIFF-%s.csv" % (time.time())
-diffDF.write.format('csv').options(header='false').save(diff)
+singlesync = "s3a://sparkoutput/output-single-SYNC.csv"
+#dfARCSYNC.write.format('csv').mode("overwrite").options(header='false').save(singlesync)
+
+
+df2 = dfARCSYNC.withColumnRenamed("_c1","digkey")
+
+inner_join_true =  dfnew.join(df2,["digkey"], "left_semi").withColumn('is_present', F.lit(True)).select('key','size','is_present').dropDuplicates()
+inner_join_false =  dfnew.join(df2,["digkey"], "leftanti").withColumn('is_present_false', F.lit(False)).select('key','is_present_false').dropDuplicates()
+
+
+print inner_join_true.show(20,False)
+print inner_join_false.show(20,False)
+
+inner_join_false = inner_join_false.withColumnRenamed("key","false_key")
+df_final = inner_join_true.join(inner_join_false, inner_join_true.key == inner_join_false.false_key,"left")
+print df_final.show(10,False)
+columns_to_drop = ['is_present_false', 'is_present','false_key']
+df_all = df_final.withColumn('good_state', F.when( ( df_final.is_present_false == 'false' ),False).otherwise(True)).drop(*columns_to_drop)
+print df_all.show(10,False)
+
+all = "s3a://sparkoutput/output-FILE-SHAPE.csv"
+df_all.write.format('csv').mode("overwrite").options(header='false').save(all)
 
