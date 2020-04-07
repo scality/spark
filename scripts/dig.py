25238063b57ed9b382c4c8cb1e4b84530bfb6751
patrick Dos Santos <patrickdos@free.fr>
2020-04-08 00:28:29 +0200

    fix padding on bin to hex

--- scripts/dig.py
+++ scripts/dig.py
@@ -2,6 +2,10 @@ import hashlib
 import binascii
 import struct
 
+def pad2(n):
+  x = '%s' % (n,)
+  return ('0' * (len(x) % 2)) + x
+
 def to_bytes(h):
         return binascii.unhexlify(h) 
 
@@ -19,21 +23,19 @@ def get_dig_key(name):
             digest[7] << 24 |  digest[8] << 16  | digest[9] << 8 | digest[10] 
       hash_str = "{0:x}".format(hash_str)
       oid = "{0:x}".format(oid)
-      print hash_str,oid
       oid = oid.zfill(16)
       volid = "00000000"
       svcid = "51"
       specific = "102060"
       cls = "70"
       key = hash_str.upper() + oid.upper() + volid + svcid + specific + cls
-      print key
       return key.zfill(40)
 
 def gen_md5_from_id(key):
+	key = key.lstrip('0')
+	key = pad2(key)
 	int_b = to_bytes(key)
-	return get_dig_key(int_b)
-
-
+	return get_dig_key(int_b).upper()
 
 lst = {}
 lst["DFB745B781FD8F5F5E832D11B257AB4D18E17720"] = "4A121AC61828A937E32EAF000000005110206070"
@@ -44,9 +46,16 @@ lst["A05AC5A8898431110B10436178757E4D4AEF9C20"] = "4E4982A2413CE2B1AC585B0000000
 lst["C5FEF5E77AAA4880A91D8CF5050EB84DB4B47300"] = "DF21350624968C7B6AC4A5000000005110206070"
 lst["3632122526CF764735B024EB3ED54DCC2E5900"] = "6B6AEDCC8B0E9E59CCCA14000000005110206070"
 lst["7076C0E2B0F13D7FE8A566FBAB714DB9B1CF00"] = "6E33D103B9CA3319F785D4000000005110206070"
-lst["09793B06BA9006D0E24CE26B8A02DC4D68A42900"] = "F8A85FB4A2F5B08B1F8494000000005110206070"
+lst["9793B06BA9006D0E24CE26B8A02DC4D68A42900"] = "F8A85FB4A2F5B08B1F8494000000005110206070"
 lst["E5A6F86148A0A9AB055966822D454DD018BF00"] = "EE71CCFB675BEB92A3687E000000005110206070"
+lst["35EC2ECEE4AD0EE6925A81CE25250801B28620"] = "DB8AC8E4E99B10324AA389000000005110206070"
 lst["122526CF764735B024EB3ED54DCC2E5900"] = "55A0249F5F59A3AF58AB79000000005110206070"
+lst["0035EC2ECEE4AD0EE6925A81CE25250801B28620"] = "DB8AC8E4E99B10324AA389000000005110206070"
+lst["000000122526CF764735B024EB3ED54DCC2E5900"] = "55A0249F5F59A3AF58AB79000000005110206070"
+
 for i in lst:
+	print i
 	print gen_md5_from_id(i) , lst[i]
 	assert gen_md5_from_id(i) == lst[i]
+gen_md5_from_id("35EC2ECEE4AD0EE6925A81CE25250801B28620")
+gen_md5_from_id("0035EC2ECEE4AD0EE6925A81CE25250801B28620")
