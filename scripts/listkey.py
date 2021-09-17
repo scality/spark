bf5c4acee948e5893d6d7ecb304b84bd81c0aace
trevorbenson <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>
2021-09-17 15:15:44 -0700

    debug cleanup

--- scripts/listkey.py
+++ scripts/listkey.py
@@ -79,66 +79,54 @@ def prepare_path():
         os.makedirs(path)
 
 def listkeys(row, now):
-        klist = []
-        n = DaemonFactory().get_daemon("node",login=user, passwd=password, url='https://{0}:{1}'.format(row.ip, row.adminport), chord_addr=row.ip, chord_port=row.chordport, dso=RING)
+    # klist = []
+    n = DaemonFactory().get_daemon("node",login=user, passwd=password, url='https://{0}:{1}'.format(row.ip, row.adminport), chord_addr=row.ip, chord_port=row.chordport, dso=RING)
+    fname = "%s/node-%s-%s.csv" % (path , row.ip, row.chordport )
+    if prot == 'file':
+        f = open(fname,"w+")
+    elif prot == 's3a':
+        f = s3.open(fname, "ab")
+    params = { "mtime_min":"123456789","mtime_max":now,"loadmetadata":"browse"}
+    for k in n.listKeysIter(extra_params=params):
+        if len(k.split(",")[0]) > 30 :
+            #klist.append([k.rstrip().split(',')[i] for i in [0,1,2,3] ])
+            data = [ k.rstrip().split(',')[i] for i in [0,1,2,3] ]
+            data.append(str(row.ip))
+            data.append(str(row.chordport))
+            data.append(str(row.name))
+            # print("Going to check if data key: " + data[0])
+            if not re.search(arcdatakeypattern, data[0]):
+                print("Regex did not match: " + data[0])
+            else:
+                print("Regex match!")
+                stat = n.chunkapiStoreOp(op='stat', key=data[0], dso=RING, extra_params={'use_base64': '1'})
+                for s in stat.findall("result"):
+                    status = s.find("status").text
+                    if status == "CHUNKAPI_STATUS_OK":
+                        usermd = s.find("usermd").text
+                        if usermd is not None:
+                            use_base64 = False
+                            try:
+                                use_base64 = s.find("use_base64").text
+                                if int(use_base64) == 1:
+                                    use_base64 = True
+                            except:
+                                pass
+                            if use_base64 is True:
+                                rawusermd = base64.b64decode(usermd)
+                                objectkeyinbytes = struct.unpack(">BBBBBBBBBBBBBBBBBBBB", rawusermd[21:41])
+                                objectkeylist = []
+                                for x in objectkeyinbytes:
+                                    raw = '{:02X}'.format(x)
+                                    objectkeylist.append(raw)
+                                objectkey = ''.join(objectkeylist)
+                                data.append(str(objectkey))
+                        else:
+                            data.append('')
+            data = ",".join(data)
+            print >> f , data
 
-        fname = "%s/node-%s-%s.csv" % (path , row.ip, row.chordport )
-        if prot == 'file':
-            f = open(fname,"w+")
-        elif prot == 's3a':
-            f = s3.open(fname, "ab")
-        params = { "mtime_min":"123456789","mtime_max":now,"loadmetadata":"browse"}
-        for k in n.listKeysIter(extra_params=params):
-            if len(k.split(",")[0]) > 30 :
-                #klist.append([k.rstrip().split(',')[i] for i in [0,1,2,3] ])
-                data = [ k.rstrip().split(',')[i] for i in [0,1,2,3] ]
-                # print("Going to check if data key: " + data[0])
-                if not re.search(arcdatakeypattern, data[0]):
-                    print("Regex did not match: " + data[0])
-                else:
-                    print("Regex match!")
-                    stat = n.chunkapiStoreOp(op='stat', key=data[0], dso=RING, extra_params={'use_base64': '1'})
-                    # print(stat)
-                    for s in stat.findall("result"):
-                        status = s.find("status").text
-                        # print("Status: " + status)
-                        if status == "CHUNKAPI_STATUS_OK":
-                            # print("Status: " + str(status))
-                            usermd = s.find("usermd").text
-                            if usermd is not None:
-                                print("Encoded usermd: " + usermd)
-                                use_base64 = False
-                                try:
-                                    use_base64 = s.find("use_base64").text
-                                    if int(use_base64) == 1:
-                                        use_base64 = True
-                                except:
-                                    pass
-                                if use_base64 is True:
-                                    print("Use base64 is true")
-                                    rawusermd = base64.b64decode(usermd)
-                                    print("Raw usermd: " + rawusermd)
-                                    # usermd = base64.b64decode(usermd).decode("iso8859-1")
-                                    objectkeyinbytes = struct.unpack(">BBBBBBBBBBBBBBBBBBBB", rawusermd[21:41])
-                                    objectkeylist = []
-                                    for x in objectkeyinbytes:
-                                        raw = '{:02X}'.format(x)
-                                        objectkeylist.append(raw)
-                                    objectkey = ''.join(objectkeylist)
-                                    print objectkey
-                                    data.append(str(objectkey))
-                            # for elem in stat.iter():
-                            #         if elem.tag == 'usermd':
-                            #                 for md in elem.iter():
-                            #                         data.append(md.text)
-                            else:
-                                data.append('')
-                #data.append(str(row.chordport))
-                #data.append(str(row.name))
-                data = ",".join(data)
-                print >> f , data
-
-        return [( row.ip, row.adminport, 'OK')]
+    return [( row.ip, row.adminport, 'OK')]
 
 now = int(str(time.time()).split('.')[0]) - retention
 prepare_path()
