6c3c1e63e9888fce564623a7efe79b351202e7c4
scality-trevorbenson <trevor.benson@scality.com>
2022-03-01 10:07:47 -0800

    Removed hardcoded 60 cores per cluster. Calculate the value dynamically from the configuration.

--- scripts/submit.py
+++ scripts/submit.py
@@ -21,11 +21,12 @@ script=options.script
 opt=options.ring
 opt2=options.extra
 localdir = "%s/tmp/" % "/var"
+total_cores = int(cfg["spark.executor.instances"]) * int(cfg["spark.executor.cores"])
 
 cmd = "./spark-2.4.3-bin-hadoop2.7/bin/spark-submit --master %s \
         --driver-memory=10g \
         --executor-memory=10g \
-	--total-executor-cores=60 \
+	--total-executor-cores=%s \
 	--conf spark.hadoop.fs.s3a.impl=org.apache.hadoop.fs.s3a.S3AFileSystem \
 	--conf spark.hadoop.fs.s3a.access.key=%s \
 	--conf spark.hadoop.fs.s3a.secret.key=%s \
@@ -36,6 +37,6 @@ cmd = "./spark-2.4.3-bin-hadoop2.7/bin/spark-submit --master %s \
 	--conf spark.local.dir=%s \
         --jars file:/root/spark/aws-java-sdk-1.7.4.jar,file:/root/spark/hadoop-aws-2.7.3.jar \
         --driver-class-path=/root/spark/aws-java-sdk-1.7.4.jar:/root/spark/hadoop-aws-2.7.3.jar \
-	./%s %s %s" % ( cfg["master"], cfg["s3"]["access_key"] , cfg["s3"]["secret_key"] , cfg["s3"]["endpoint"] , localdir, script , opt, opt2 )
+	./%s %s %s" % ( cfg["master"], total_cores, cfg["s3"]["access_key"] , cfg["s3"]["secret_key"] , cfg["s3"]["endpoint"] , localdir, script , opt, opt2 )
 
 os.system(cmd)
