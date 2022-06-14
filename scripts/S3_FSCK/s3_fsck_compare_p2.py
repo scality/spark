2fc22f83d06ae0d12ea05df877a1ae927ad34345
scality-trevorbenson <trevor.benson@scality.com>
2022-06-14 10:55:37 -0700

    [VERIFICATION] - Compare the output of an original P2 run with a subsequent one

---
+++ scripts/S3_FSCK/s3_fsck_compare_p2.py
@@ -0,0 +1,53 @@
+from pyspark.sql import SparkSession, Row, SQLContext
+import pyspark.sql.functions as F
+from pyspark import SparkContext
+import os
+import sys
+import requests
+
+import yaml
+
+config_path = "%s/%s" % ( sys.path[0], "../config/config.yml")
+with open(config_path, "r") as ymlfile:
+    cfg = yaml.load(ymlfile)
+
+if len(sys.argv) >1:
+    RING = sys.argv[1]
+else:
+    RING = cfg["ring"]
+
+PATH = cfg["path"]
+PROTOCOL = cfg["protocol"]
+ACCESS_KEY = cfg["s3"]["access_key"]
+SECRET_KEY = cfg["s3"]["secret_key"]
+ENDPOINT_URL = cfg["s3"]["endpoint"]
+
+os.environ["PYSPARK_SUBMIT_ARGS"] = '--packages "org.apache.hadoop:hadoop-aws:2.7.3" pyspark-shell'
+spark = SparkSession.builder \
+     .appName("s3_fsck_p2.py:Union the S3 keys and the RING keys :" + RING) \
+     .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")\
+     .config("spark.hadoop.fs.s3a.access.key", ACCESS_KEY)\
+     .config("spark.hadoop.fs.s3a.secret.key", SECRET_KEY)\
+     .config("spark.hadoop.fs.s3a.endpoint", ENDPOINT_URL) \
+     .config("spark.executor.instances", cfg["spark.executor.instances"]) \
+     .config("spark.executor.memory", cfg["spark.executor.memory"]) \
+     .config("spark.executor.cores", cfg["spark.executor.cores"]) \
+     .config("spark.driver.memory", cfg["spark.driver.memory"]) \
+     .config("spark.memory.offHeap.enabled", cfg["spark.memory.offHeap.enabled"]) \
+     .config("spark.memory.offHeap.size", cfg["spark.memory.offHeap.size"]) \
+     .config("spark.local.dir", cfg["path"]) \
+     .getOrCreate()
+
+
+prior = "%s://%s/%s/s3fsck/history/s3objects-missing.csv" % (PROTOCOL, PATH, RING)
+current = "%s://%s/%s/s3fsck/s3objects-missing.csv" % (PROTOCOL, PATH, RING)
+
+df1 = spark.read.format("csv").option("header", "false").option("inferSchema", "true").load(prior)
+df2 = spark.read.format("csv").option("header", "false").option("inferSchema", "true").load(current)
+
+df2_renamed = df2.withColumnRenamed("_c0", "_c0_renamed")
+
+df_final = df1.join(df2_renamed, df1['_c0'] == df2_renamed['_c0_renamed'], "leftanti").select(df1['_c0'])
+allmissing = "%s://%s/%s/s3fsck/verification/p2.csv" % (PROTOCOL, PATH, RING)
+df_final.write.format("csv").mode("overwrite").options(header="false").save(allmissing)
+
