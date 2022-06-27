b0c14bf1cb28a49c43116b50315ad89c146b435c
scality-trevorbenson <trevor.benson@scality.com>
2022-06-27 16:24:47 -0700

    Working s3_fsck_compare_p2_to_p0.py

--- scripts/S3_FSCK/s3_fsck_compare_p2_to_p0.py
+++ scripts/S3_FSCK/s3_fsck_compare_p2_to_p0.py
@@ -1,21 +1,19 @@
 from pyspark.sql import SparkSession, Row, SQLContext
-
 import pyspark.sql.functions as F
-
 from pyspark import SparkContext
-
 import os
-
 import sys
-
 import requests
-
 import yaml
 
 config_path = "%s/%s" % ( sys.path[0], "../config/config.yml")
 
+# USED FOR TESTING IN PYTHON CONSOLE
+# config_path = "%s/%s" % ( sys.path[0], "/root/spark/scripts/config/config.yml")
+
 with open(config_path, "r") as ymlfile:
     cfg = yaml.load(ymlfile)
+
 if len(sys.argv) > 1:
     RING = sys.argv[1]
 else:
@@ -54,15 +52,15 @@ digkeys = "%s://%s/%s/s3fsck/s3-dig-keys.csv" % (PROTOCOL, PATH, RING)
 
 orphans = "%s://%s/%s/s3fsck/s3objects-missing.csv" % (PROTOCOL, PATH, RING)
 
-df_p0 = spark.read.format("csv").option("header", "false").option("inferSchema", "true").load(digkeys)
+df_p0 = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load(digkeys)
 
-df_p2 = spark.read.format("csv").option("header", "false").option("inferSchema", "true").load(orphans)
+df_p2_tmp = spark.read.format("csv").option("header", "false").option("inferSchema", "true").load(orphans)
 
-df_p2_renamed = df_p2.withColumnRenamed("_c0", "_c0_renamed")
+df_p2_renamed = df_p2_tmp.withColumnRenamed("_c0", "p2_ringkey")
 
-# df_final = df1.join(df2_renamed, df1['_c0'] == df2_renamed['_c0_renamed'], "leftanti").select(df1['_c0'])
-df_final = df_p2_renamed.join(df_p0, df_p2_renamed['_c0_renamed'].rlike(df_p0['_c0'])).select(df_p2_renamed['_c0_renamed'])
-# df_final = df1.join(df2, array_contains(split(df2("_c0_renamed"), "left"), df1("_c0")))
+df_p2 = df_p2_renamed.withColumn("p2_digkey", F.substring("p2_ringkey", 0, 26))
+
+df_final = df_p2.join(df_p0, df_p2['p2_digkey'] == df_p0['digkey']).select(df_p2_renamed['p2_ringkey'])
 
 allmissing = "%s://%s/%s/s3fsck/verification/compare_p2_to_p0.csv" % (PROTOCOL, PATH, RING)
 df_final.write.format("csv").mode("overwrite").options(header="false").save(allmissing)
