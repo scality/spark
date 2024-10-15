c0b008f675bd90239db6238ea5bafaccdd8ce836
francois.nomarski <francois.nomarski@scality.com>
2024-10-15 23:40:53 +0200

    add EL8 CTR support to export_s3_keys.sh

--- scripts/S3_FSCK/export_s3_keys.sh
+++ scripts/S3_FSCK/export_s3_keys.sh
@@ -2,6 +2,8 @@
 
 RID=$1
 export WORKDIR=/var/tmp/bucketSproxydKeys
+container_command="$(basename "$(type -p docker || type -p ctr)")"
+test -z "$container_command" && echo "docker or CTR not found!" && exit 1
 
 if ! [ -d "${WORKDIR}" ]
 then
@@ -12,18 +14,38 @@ for bucket in $(curl --silent http://localhost:9000/_/raft_sessions/${RID}/bucke
 
 do
     echo "--- Starting on ${bucket} ---"
-    docker run \
-        --rm \
-        -it \
-        --net host \
-        --entrypoint /usr/local/bin/node \
-        -e 'BUCKETD_HOSTPORT=127.0.0.1:9000' \
-        -e "BUCKETS=${bucket}" \
-        -e 'NO_MISSING_KEY_CHECK=1' \
-        -e 'VERBOSE=1' \
-        ghcr.io/scality/s3utils:1.14.6 \
-        verifyBucketSproxydKeys.js  \
-        > ${WORKDIR}/raw_${bucket}_keys.txt
+    case $container_command
+        in
+        docker) echo "Running $container_command"
+           $container_command run \
+               --rm \
+               -it \
+               --net host \
+               --entrypoint /usr/local/bin/node \
+               -e 'BUCKETD_HOSTPORT=127.0.0.1:9000' \
+               -e "BUCKETS=${bucket}" \
+               -e 'NO_MISSING_KEY_CHECK=1' \
+               -e 'VERBOSE=1' \
+               registry.scality.com/s3utils/s3utils:1.14.101 \
+               verifyBucketSproxydKeys.js  \
+               > ${WORKDIR}/raw_${bucket}_keys.txt
+        ;;
+        ctr) echo "Running $container_command"
+            $container_command run \
+                --rm \
+                --net-host \
+                --mount='type=bind,src=/root/spark-apps,dst=/opt/spark/apps,options=rbind:rw' \
+                -cwd=/usr/src/app \
+                --env 'BUCKETD_HOSTPORT=127.0.0.1:9000' \
+                --env "BUCKETS=${bucket}" \
+                --env 'NO_MISSING_KEY_CHECK=1' \
+                --env 'VERBOSE=1' \
+                registry.scality.com/s3utils/s3utils:1.14.101 \
+                EXPORT_S3_KEYS \
+                /usr/local/bin/node verifyBucketSproxydKeys.js \
+                > ${WORKDIR}/raw_${bucket}_keys.txt
+        ;;
+    esac
 
     echo "--- Processing output of ${bucket}... ---"
     jq -r 'select(.message | contains("sproxyd key"))| [(.objectUrl | split("s3://") | .[1] | split("/") | .[0]),(.objectUrl | split("s3://") | .[1] | split("/") | .[-1]),.sproxydKey]| @csv' ${WORKDIR}/raw_${bucket}_keys.txt > ${WORKDIR}/${bucket}_keys.txt
